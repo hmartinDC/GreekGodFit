102000bc655a67af496513fddd9d27911e056f8e
hmartinDC <hmartinreina@cifpfbmoll.eu>
2023-01-27 19:51:49 +0100

    Checkpoint

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import { RouterLink, RouterView } from "vue-router";
 import HeaderGreek from "./components/HeaderGreek.vue";
 import WelcomeGreek from "./components/WelcomeGreek.vue";
+import FooterGreek from "./components/FooterGreek.vue";
 </script>
 
 <template>
   <HeaderGreek />
   <WelcomeGreek />
+  <FooterGreek />
 </template>
